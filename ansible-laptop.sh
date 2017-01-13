1e06c5772f246ae72db6a78a9bf582b6d907b2b3
MrBiTs <mrbits.dcf@gmail.com>
2017-01-13 09:25:41 -0200

    Improving script

--- ansible-laptop.sh
+++ ansible-laptop.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-echo "Updating repositories"
-sudo apt -y update
+CURRENT=$(pwd)
+USER=$(whoami)
 
-echo "Installing PIP"
-sudo apt -y install python-pip && sudo pip install -U pip
+if [ ! -d ~/.ansible-laptop] then
+    echo "\033[0;32m Python install \033[0m"
+    sudo apt-get install -y -qq python python-pip git git-core openssh-server
 
-echo "Installing ansible"
-sudo pip install ansible
+    echo "\033[0;32m Updating pip \033[0m"
+    sudo pip install -U pip
 
-ansible-playbook ansible-laptop.yml
+    echo "\033[0;32m Ansible Installation \033[0m"
+    sudo pip install ansible
+
+    echo "\033[0;32m Clone repository \033[0m"
+    sudo -u `whoami` -H git clone git@github.com:mrbitsdcf/ansible-laptop.git $HOME/.ansible-laptop
+else
+    echo "\033[0;32mThe folder ansible-laptop is already installed \033[0m"
+fi
+
+cd $HOME/.ansible-laptop
+
+ansible-playbook -i hosts ansible-laptop.yml -c local -K
